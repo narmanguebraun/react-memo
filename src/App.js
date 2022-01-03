d9258218b1ddd2e8c6de4095ec4b5ba8dfa33a5c
narmanguebraun <narmanguebraun1@gmail.com>
2022-01-03 20:31:56 +0100

    shuffle cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+import { useState } from 'react';
 import './App.css';
 
+const cardImages = [
+  { "src": "/img/01.jpg" },
+  { "src": "/img/02.jpg" },
+  { "src": "/img/03.jpg" },
+  { "src": "/img/04.jpg" },
+  { "src": "/img/05.jpg" },
+  { "src": "/img/06.jpg" },
+  { "src": "/img/07.jpg" },
+  { "src": "/img/08.jpg" },
+  { "src": "/img/09.jpg" }
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+  
+      setCards(shuffledCards)
+      setTurns(0)
+  }
+
+  console.log(cards, turns)
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
