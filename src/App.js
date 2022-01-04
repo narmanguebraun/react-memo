3fd034d1e8dd4b70aaa10cb622938b5c737cd893
narmanguebraun <narmanguebraun1@gmail.com>
2022-01-04 12:19:35 +0100

    start game

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }))
   
+      setChoiceOne(null)
+      setChoiceTwo(null)
       setCards(shuffledCards)
       setTurns(0)
   }
@@ -69,6 +71,11 @@ function App() {
     setDisabled(false)
   }
 
+  // start a new game automatically
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -85,6 +92,7 @@ function App() {
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
